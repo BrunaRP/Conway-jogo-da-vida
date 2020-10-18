2d82305837cf3df1d9bf672b60b2a25e554bb2c6
BrunaRP <polonio.bruna@gmail.com>
2020-10-18 17:59:28 -0300

    planejamento

---
+++ index.js
@@ -0,0 +1,17 @@
+/*a fazer 
+html:
+canva
+células começando em lugares aleatórios
+botão começar e parar
+
+css:
+tudo
+
+index.js
+chamar a classe do jogo
+
+classe jogo da vida.js
+lógica do jogo
+variaveis(2 arrays vazios)
+funções
+*/ 
